37c2bbb9db23a2a127dfbd3238f543452ef15167
Ekadyah <ekadyah1604@gmail.com>
2024-02-19 19:57:46 +0700

    tes

--- application/libraries/Template.php
+++ application/libraries/Template.php
@@ -1,17 +1,11 @@
-<?php
-class Template
-{
-    var $template_data = array();
-    function set($name, $value)
-    {
-        $this->template_data[$name] = $value;
-    }
-    
-    function load($template = '', $view = '', $view_data = array(), $return = FALSE)
-    {
-        $this->CI =& get_instance();
-        $this->set('contents', $this->CI->load->view($view, $view_data, TRUE));
-        return $this->CI->load->view($template, $this->template_data, $return);
-    }
-}
-?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+  </body>
+</html>
